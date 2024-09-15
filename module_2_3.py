d4022fbe6d966c014698437f52f7ae0358cf9feb
Tserya95 <110558474+Tserya95@users.noreply.github.com>
2024-09-15 15:43:59 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,13 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+len_my_list = len(my_list)
+
+
+while 1 > 0:
+    for i in my_list:
+
+        if i > 0:
+            print(i)
+        if i < 0:
+            break
+    break
+
